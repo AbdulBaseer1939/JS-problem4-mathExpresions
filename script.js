9bae6aa485541948b906accc8a20b469637435a5
AbdulBaseer <160875997+AbdulBaseer1939@users.noreply.github.com>
2024-12-20 10:39:08 +0500

    Update scriptUserTable.js

--- script.js
+++ script.js
@@ -52,7 +52,34 @@ for (var i = 1; i <= 10; i++) {
 
 document.write("Multiplication Table of " + inputNumber + ":<br>" + multiplicationTable);
 
+// Problem: 
+// a) Take three subjects name from user and store them in 3 different variables. 
+// b) Total marks for each subject is 100, store it in another variable. 
+// c) Take obtained marks for first subject from user and stored it in different variable. 
+// d) Take obtained marks for remaining 2 subjects from user and store them in variables. 
+// e) Now calculate total marks and percentage and show the result in browser like this.
+// ! (Hint: user table) 
 
+    var subject1 = prompt("Enter the name of first subject");
+    var subject2 = prompt("Enter the name of second subject");
+    var subject3 = prompt("Enter the name of third subject");
+
+    var totalMarks = 100;
+    var subject1Marks = prompt("Enter marks obtained in " + subject1);
+    var subject2Marks = prompt("Enter marks obtained in " + subject2);
+    var subject3Marks = prompt("Enter marks obtained in " + subject3);
+    
+    var totalObtainedMarks = parseInt(subject1Marks) + parseInt(subject2Marks) + parseInt(subject3Marks);
+    var percentage = (totalObtainedMarks / totalMarks) * 100;
+    
+    document.write("<table border='1'>");
+    document.write("<tr><th>Subject</th><th>Marks Obtained</th><th>Total Marks</th></tr>");
+    document.write("<tr><td>" + subject1 + "</td><td>" + subject1Marks + "</td><td>" + totalMarks + "</td></tr>");
+    document.write("<tr><td>" + subject2 + "</td><td>" + subject2Marks + "</td><td>" + totalMarks + "</td></tr>");
+    document.write("<tr><td>" + subject3 + "</td><td>" + subject3Marks + "</td><td>" + totalMarks + "</td></tr>");
+    document.write("<tr><td colspan='2'>Total Obtained Marks</td><td>" + totalObtainedMarks + "</td></tr>");
+    document.write("<tr><td colspan='2'>Percentage</td><td>" + percentage + "%</td></tr>");
+    document.write("</table>");
 
 
 
